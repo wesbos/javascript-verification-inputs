0492bc606cc8e65d43c43eed34ecd28adbab3435
Wes Bos <wesbos@gmail.com>
2020-10-22 14:09:30 -0400

    add backspace challenge

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ inputs[0].addEventListener('paste', handlePaste);
 
 form.addEventListener('input', handleInput);
 
-// 123456
-// 938423
-// 938
+// 1. select the text when the next input is focued
+// 2. Auto submit the form if all fields are filled after a paste
+// 3. support for backspacing from 1 input to another
